89c7b0c5bd99cb71ae8e8b78c890cde9f5da34c3
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2021-02-19 11:36:37 +0500

    install and configure react-context-devtool for real time debugging context state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { debugContextDevtool } from "react-context-devtool";
+
+const container = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  container
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// configure react context devtool for development
+let options = {
+  debugReducer: true, //by default true
+  debugContext: true, //by default true
+};
+
+debugContextDevtool(container, options);
