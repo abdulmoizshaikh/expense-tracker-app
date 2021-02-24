640f4b81432b4b6cdc0ece14d231e710de624cb0
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2021-02-24 15:31:12 +0500

    BalanceInquiry done

--- src/views/components/BalanceInquiry/BalanceInquiry.js
+++ src/views/components/BalanceInquiry/BalanceInquiry.js
@@ -1,35 +1,24 @@
 import React, { Component } from "react";
 import "./BalanceInquiry.css";
 
-/**
- *  flux using in class component
- */
-
-class BalanceInquiry extends Component {
+export default class BalanceInquiry extends Component {
   render() {
-    // console.log("this.props in BalanceInquiry", this.props);
-    // return (
-    // {(state) => {
     // nested array destructuring with default values
-    // const {
-    //   income: [totalIncome, setTotalIncome],
-    //   expense: [totalExpense, setTotalExpense],
-    // } = state;
+    const {
+      state: { totalIncome, totalExpense },
+    } = this.props;
+
     return (
       <div className="container">
         <span className="text">Your balance</span>
         <br />
         <span className="text">
-          {/* $
-              {totalIncome + totalExpense !== 0
-                ? totalIncome + totalExpense + ".00"
-                : totalIncome + totalExpense} */}
+          $
+          {totalIncome + totalExpense !== 0
+            ? totalIncome + totalExpense + ".00"
+            : totalIncome + totalExpense}
         </span>
       </div>
     );
-    // }}
-    // );
   }
 }
-
-export default BalanceInquiry;
