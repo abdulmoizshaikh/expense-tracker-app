77a7d25de3661f4ec6b36ae0fdd7736e1e8d2f40
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2021-02-24 15:28:01 +0500

    IncomeExpenseCard work done

--- src/views/components/IncomeExpenseCard/IncomeExpenseCard.js
+++ src/views/components/IncomeExpenseCard/IncomeExpenseCard.js
@@ -1,26 +1,22 @@
 import "./IncomeExpenseCard.css";
 
-/*
- context using in functional component
- */
 const IncomeExpenseCard = (props) => {
   // nested array destructuring with default values
-  // const {
-  //   income: [totalIncome = 0, setTotalIncome = {}],
-  //   expense: [totalExpense = 0, setTotalExpense = {}],
-  // } = state;
-  // console.log("props in IncomeExpenseCard", props);
+  const {
+    state: { totalIncome = 0, totalExpense = 0 },
+  } = props;
+
   return (
     <div className="container">
       <div id="card">
         <div className="cardItem">
           <p>INCOME</p>
-          {/* <p id="income">{totalIncome}</p> */}
+          <p id="income">{totalIncome}</p>
         </div>
         <p id="v-line" />
         <div className="cardItem">
           <p>EXPENSE</p>
-          {/* <p id="expense">{totalExpense}</p> */}
+          <p id="expense">{totalExpense}</p>
         </div>
       </div>
     </div>
